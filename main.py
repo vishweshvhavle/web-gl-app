3a9f423839e2688d07aac0c38008dceb1b52bd82
vishweshvhavle <vishweshvhavle@gmail.com>
2024-06-22 14:47:01 +0530

    chore: Update app.yaml and firebase.json configurations

---
+++ main.py
@@ -0,0 +1,105 @@
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit
+
+from threading import Lock
+import sys
+import numpy as np
+import json
+
+# some dummy data
+data = [{'foo': [1, 2, 3, 4], 'fee': 'hello'}]
+data_json = json.dumps(data)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+thread = None
+thread_lock = Lock()
+
+# global variables to hold the params and camera
+params = None
+updateParams = False
+camera = None
+updateCamera = False
+controls = None
+updateControls = False
+
+# number of seconds between updates
+seconds = 0.01
+
+# this will pass to the viewer every "seconds"
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    global params, updateParams, camera, updateCamera, controls, updateControls
+    while True:
+        socketio.sleep(seconds)
+        if updateParams:
+            print("========= params:", params)
+            socketio.emit('update_params', params, namespace='/test')
+        if updateCamera:
+            socketio.emit('update_camera', camera, namespace='/test')
+        if updateControls:
+            socketio.emit('update_controls', controls, namespace='/test')
+        updateParams = False
+        updateCamera = False
+        updateControls = False
+
+# testing the connection
+@socketio.on('connection_test', namespace='/test')
+def connection_test(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('connection_response', {'data': message['data'], 'count': session['receive_count']})
+
+# sending data
+@socketio.on('input_data_request', namespace='/test')
+def input_data_request(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('input_data_response', data_json)
+
+# will receive data from gui (and print to console as a test within "from_gui")
+@socketio.on('gui_input', namespace='/test')
+def gui_input(message):
+    global params, updateParams
+    updateParams = True
+    params = message
+
+# will receive data from camera
+@socketio.on('camera_input', namespace='/test')
+def camera_input(message):
+    global camera, updateCamera
+    updateCamera = True
+    camera = message
+
+# will receive data from controls
+@socketio.on('controls_input', namespace='/test')
+def controls(message):
+    global controls, updateControls
+    updateControls = True
+    controls = message
+
+# the background task sends data to the viewer
+@socketio.on('connect', namespace='/test')
+def from_gui():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+
+##############
+
+# flask stuff
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/viewer")
+def viewer():
+    return render_template("viewer.html")
+
+@app.route("/gui")
+def gui():
+    return render_template("gui.html")
+
+socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+
